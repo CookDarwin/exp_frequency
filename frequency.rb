2fc5229361b57c0701e96c0ae10625bc85d3e2ad
Cook.Darwin <cook_darwin@hotmail.com>
2021-01-02 23:24:36 +0800

    demo complete

---
+++ frequency.rb
@@ -0,0 +1,75 @@
+require_relative "../tdl/tdl.rb"
+TdlBuild.frequency(__dir__) do 
+    input   - 'in_signal'
+    input   - 'clock'
+    input   - 'rst_n'
+    output.logic[16]  - 'freq'
+    output.logic[16]  - 'phase'
+
+    ## 定义采集窗口
+    localparam.TOTAL    (2<<16)
+    logic[16]   - 'win_cnt'
+    logic       - 'win_sample'
+
+    always_ff(posedge.clock, negedge.rst_n) do 
+        IF ~rst_n do 
+            win_cnt     <= 0.A 
+            win_sample  <= 1.b0 
+        end
+        ELSE do 
+            win_cnt     <= win_cnt  + 1.b1 
+            win_sample  <= win_cnt == 1.A 
+        end
+    end
+
+    logic[16]   - 'high_cnt'
+    logic[16]   - 'low_cnt'
+
+    always_ff(posedge.clock, negedge.rst_n) do 
+        IF ~rst_n do 
+            high_cnt    <= 0.A 
+            low_cnt     <= 0.A 
+        end
+        ELSE do 
+            IF win_sample do 
+                high_cnt    <= 0.A 
+                low_cnt     <= 0.A 
+            end
+            ELSE do 
+                high_cnt    <= high_cnt + in_signal
+                low_cnt     <= low_cnt  + (~in_signal)
+            end
+        end
+    end
+
+    ## base cnt
+    logic[16]   - 'scnt'
+
+    always_ff(posedge.in_signal,negedge.rst_n) do 
+        IF ~rst_n do 
+            scnt    <= 0.A 
+        end
+        ELSE do 
+            scnt    <= scnt
+        end
+    end
+
+    ## 锁结果
+    always_ff(posedge.in_signal,negedge.rst_n) do 
+        IF ~rst_n do 
+            freq    <= 0.A 
+            phase   <= 0.A 
+        end
+        ELSE do 
+            
+            IF win_sample do 
+                phase   <= high_cnt
+                freq    <= scnt.cross_clock(clock: clock)
+            end
+            ELSE do 
+                phase   <= phase
+                freq    <= freq
+            end
+        end
+    end
+end
---
+++ frequency.sv
@@ -0,0 +1,103 @@
+/**********************************************
+_______________________________________ 
+___________    Cook Darwin   __________    
+_______________________________________
+descript:
+author : Cook.Darwin
+Version: VERA.0.0
+created: xxxx.xx.xx
+madified:
+***********************************************/
+`timescale 1ns/1ps
+
+module frequency (
+    input              in_signal,
+    input              clock,
+    input              rst_n,
+    output logic[15:0] freq,
+    output logic[15:0] phase
+);
+
+//==========================================================================
+//-------- define ----------------------------------------------------------
+localparam  TOTAL  = 131072;
+logic [16-1:0]  win_cnt ;
+logic win_sample;
+logic [16-1:0]  high_cnt ;
+logic [16-1:0]  low_cnt ;
+logic [16-1:0]  scnt ;
+logic [16-1:0]  scnt_cc ;
+
+//==========================================================================
+//-------- instance --------------------------------------------------------
+
+//==========================================================================
+//-------- expression ------------------------------------------------------
+always_ff@(posedge clock,negedge rst_n) begin 
+    if(~rst_n)begin
+         win_cnt <= '0;
+         win_sample <= 1'b0;
+    end
+    else begin
+         win_cnt <= ( win_cnt+1'b1);
+         win_sample <= win_cnt== '1;
+    end
+end
+
+always_ff@(posedge clock,negedge rst_n) begin 
+    if(~rst_n)begin
+         high_cnt <= '0;
+         low_cnt <= '0;
+    end
+    else begin
+        if(win_sample)begin
+             high_cnt <= '0;
+             low_cnt <= '0;
+        end
+        else begin
+             high_cnt <= ( high_cnt+in_signal);
+             low_cnt <= ( low_cnt+~in_signal);
+        end
+    end
+end
+
+always_ff@(posedge in_signal,negedge rst_n) begin 
+    if(~rst_n)begin
+         scnt <= '0;
+    end
+    else begin
+         scnt <= scnt;
+    end
+end
+
+
+//----->> scnt[15:0] cross clock <<------------------
+cross_clk_sync #(
+	.LAT   (2      ),
+	.DSIZE (16)
+)scnt_cross_clk_inst(
+/* input              */ .clk       (clock),
+/* input              */ .rst_n     (1'b1),
+/* input [DSIZE-1:0]  */ .d         (scnt),
+/* output[DSIZE-1:0]  */ .q         (scnt_cc)
+);
+//-----<< scnt[15:0] cross clock >>------------------
+
+always_ff@(posedge in_signal,negedge rst_n) begin 
+    if(~rst_n)begin
+         freq <= '0;
+         phase <= '0;
+    end
+    else begin
+        if(win_sample)begin
+             phase <= high_cnt;
+             freq <= scnt_cc;
+        end
+        else begin
+             phase <= phase;
+             freq <= freq;
+        end
+    end
+end
+
+endmodule
